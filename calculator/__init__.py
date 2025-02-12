d570d15341f5432ef791753051da6bf30526eba8
prabhathv07 <pv342@njit.edu>
2025-02-11 22:28:02 -0500

    updated files

--- calculator/__init__.py
+++
@@ -1,8 +0,0 @@
-'''My calculator Test'''
-def add(a,b):
-    '''Test the addition function works'''
-    return a + b
-
-def subtract(a,b):
-    '''Test the subtraction function works'''
-    return a - b
